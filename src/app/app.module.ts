acb60491d534d37290e451ea0e1617a992f9080c
Jamal Uddin Ahamed <68894666+jamaluddinahamed@users.noreply.github.com>
2022-03-31 23:47:05 +0530

    Uploading Project

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MainPageComponent } from './widgets/main-page/main-page.component';
+import { GetStartedComponent } from './components/get-started/get-started/get-started.component';
+import { HeaderComponent } from './components/common/header/header.component';
+import { FooterComponent } from './components/common/footer/footer.component';
+import { AdminAuthService } from './services/auth/admin-auth/admin-auth.service';
+import { AdminAuthComponent } from './components/auth/admin-auth/admin-auth.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainPageComponent,
+    GetStartedComponent,
+    HeaderComponent,
+    FooterComponent,
+    AdminAuthComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/widgets/main-page/main-page.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main-page',
+  templateUrl: './main-page.component.html',
+  styleUrls: ['./main-page.component.css']
+})
+export class MainPageComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  //stable variables
+  title = "Final Year Project";
+  banner_text = "Final Year project has started"
+
+
+
+
+
+
+
+}
